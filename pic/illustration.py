d864b61f30c07784c519c42caeb383a576e9f28b
root <jsbyxyc@me.com>
2017-03-10 12:53:08 +0800

    add

--- pic/illustration.py
+++ pic/illustration.py
@@ -2,19 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-n_groups = 2
+n_groups = 3
 
-over = (0.415268,0.967625)
-noover = (0.421380,0.912704)
-full = (0.347465,0.954963)
+#over = (0.415268,0.967625)
+#noover = (0.421380,0.912704)
+#full = (0.347465,0.954963)
+omnetpp = (0.347465,0.415268,0.421380)
+lbm = (0.954963,0.912704,0.967625)
 
 #fig, ax = plt.subplots()
-
+plt.figure(figsize=(15,15))
 index = np.arange(n_groups)
 bar_width = 0.2
 
 index_2 = (40,80,120,160);
-opacity = 0.4
+opacity = 0.9
 ax = plt.subplot(1,2,1);
 
 ax.broken_barh([(20, 160)], (26, 1), facecolors='g')
@@ -31,37 +33,45 @@ ax.set_xlim(0, 200)
 ax.set_xlabel('scheme')
 ax.set_yticks([8.5,10.5,16.5,18.5,24.5,26.5])
 ax.set_yticklabels(['omnetpp', 'lbm','omnetpp','lbm','omnetpp','lbm'])
-ax.set_xticks([20,60,100,140,180]);
+ax.set_xticks([40,80,120,160]);
+ax.set_xticklabels([ 'w1', 'w2', 'w3', 'w4'])
+ax.set_xticks([20,60,100,140,180],minor = True)
 
 
-ax.set_xticklabels(['w0', 'w1', 'w2', 'w3', 'w4'])
-ax.grid(axis='x',linestyle='-.')
+ax.grid(axis='x',linestyle='-.',which = 'minor')
 #plt.xticks(index_2, ('w1','w2','w3','w4'))
-ax.annotate('full-contention', (62, 22),fontsize=14)
-ax.annotate('non-overlap', (62, 14),fontsize=14)
-ax.annotate('overlap', (72, 6),fontsize=14)
+ax.annotate('Full-Contention:', (21, 28),fontsize=14)
+ax.annotate('Non-Overlap:', (21, 20),fontsize=14)
+ax.annotate('Overlap:', (21, 12),fontsize=14)
 
 plt.subplot(1,2,2);
-rects1 = plt.bar(0.3+index, full, bar_width,
-                 alpha=opacity,
-                 color='blue',
-                 label='full-contention')
+#rects1 = plt.bar(0.3+index, full, bar_width,
+#                 alpha=opacity,
+#                 color='blue',
+#                 label='full-contention')
 
-rects2 = plt.bar(0.3+index+bar_width, noover, bar_width,
-                 alpha=0.4,
-                 color='red',
-                 label='non-overlap')
+#rects2 = plt.bar(0.3+index+bar_width, noover, bar_width,
+#                 alpha=0.4,
+#                 color='red',
+#                 label='non-overlap')
 
-rects3 = plt.bar(0.3+index+bar_width*2, over, bar_width,
-                 alpha=0.4,
-                 color='yellow',
-                 label='overlap')
-
-
-plt.axis([0,2,0.3,1])
+#rects3 = plt.bar(0.3+index+bar_width*2, over, bar_width,
+#                 alpha=0.4,
+#                 color='yellow',
+#                 label='overlap')
+p1 = plt.bar(0.3+index*0.5, lbm, bar_width,
+                  alpha=opacity,
+                  color='#d62728',
+                  label='omnetpp')
+p2 = plt.bar(0.3+index*0.5, omnetpp, bar_width,bottom=lbm,
+                  alpha=opacity,
+                  #color='#d62728',
+                  
+label='lbm')
+plt.axis([0.15,1.65,0.8,1.5])
 plt.xlabel('Illustration of omnetpp and lbm')
 plt.ylabel('IPC')
-plt.xticks(0.2+index + bar_width*1.5, ('omnetpp', 'lbm'))
+plt.xticks(0.3+index*0.5 + bar_width*0.5, ('full-contention', 'non-overlap','overlap'))
 #plt.legend()
 plt.legend(loc='upper left')
 
--- pic/slowdown.py
+++ pic/slowdown.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python
 import numpy as np
 import matplotlib.pyplot as plt
-
-n_groups = 10
+plt.figure(figsize=(40,10))
+n_groups = 12
 index = np.arange(n_groups)
 data = []
 target = open("./slowdown.txt")
-i = 3
+i = 0
 for line in target:
-    if i==3:
+    if i==0:
         a = line.strip().split()
         data.append(a)
-        i = 0
+        i = 1
     else:
         a = [float(x) for x in line.strip().split()]
         data.append(a)
-        i += 1
+
 #ws = (2.302215,1.173829,1.662305,1.379330,1.807729,1.804873,1.409274,1.179289,1.267336,1.625169)
 #ms = (3.617407,1.073733,1.660431,1.727669,1.471084,1.715320,1.363607,3.177756,1.657094,3.202971)
 #fs = (2.890952,1.285745,1.797710,1.368337,2.845843,2.328545,1.588748,1.229131,1.017742,2.956141)
 
-plt.figure(figsize=(20,10))
-plt.axis([-0.5,9.5,0,5])
-plt.plot(index,data[1],'ro-',label = '1');
-plt.plot(index,data[2],'go-', label = '2');
-plt.plot(index,data[3],'bo-',label = '3');
+plt.axis([-0.5,11.5,0,3.5])
+plt.plot(index,data[1],'ro-',label = 'Average Weighted Slowdown');
+plt.plot(index,data[2],'gx-', label = 'Max Weighted Slowdown');
+plt.plot(index,data[3],'bv-',label = 'Fair Weighted Slowdown');
+plt.plot(index,data[4],'ys-',label = 'Non-overlap');
+plt.plot(index,data[5],'k*-',label = 'Full contention');
+
 plt.xlabel("benchmarks");
 plt.ylabel("slowdown");
 #plt.xticks(index, ('omnetpp', 'leslie3d', 'bwaves', 'zeusmp','povray','libquantum','milc','gobmk','hmmer','sjeng'))
 plt.xticks(index,data[0])
 plt.legend(loc = 'upper left',fontsize = 'small')
+
 plt.show()
